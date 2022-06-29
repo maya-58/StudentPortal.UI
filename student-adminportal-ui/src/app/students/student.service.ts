fb6f3af473067ac646ff68a2769625c1bc892acf
maya-58 <manimacrack@gmail.com>
2022-06-29 13:02:36 +0530

    Angulat Dataloading read api

---
+++ student-adminportal-ui/src/app/students/student.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//import {Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs';
+import { Student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseAPIUrl="https://localhost:44339";
+  constructor(private httpClient:HttpClient) { }
+
+  getStudents():Observable<Student[]>{
+    return this.httpClient.get<Student[]>(this.baseAPIUrl+'/Students');
+  }
+}
---
+++ student-adminportal-ui/src/app/students/students.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
+import { StudentService } from './student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students: Student[] =[];
+  
+  //For Addng tanle Header
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
+  dataSource:MatTableDataSource<Student>=new MatTableDataSource();
+
+  //Fot Pagination
+  @ViewChild(MatPaginator) matpaginator!:MatPaginator;
+  @ViewChild(MatSort) MatSort!:MatSort;
+  filterString='';
+  constructor(private studentService:StudentService) { }
+
+  ngOnInit(): void {
+    //fetch students
+    this.studentService.getStudents()
+    .subscribe(
+      (successResponse)=>{
+        //console.log(successResponse);
+        this.students=successResponse;
+        this.dataSource=new MatTableDataSource<Student>(this.students);
+
+        if(this.matpaginator){
+          this.dataSource.paginator=this.matpaginator;
+        }
+        if(this.MatSort){
+          this.dataSource.sort=this.MatSort;
+        }
+      },
+      (errorResponse)=>{
+        console.log(errorResponse);
+      }
+
+    );
+  }
+
+  filterStudents() {
+    this.dataSource.filter = this.filterString.trim().toLowerCase();
+  }
+
+}
